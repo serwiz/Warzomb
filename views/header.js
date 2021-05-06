4f012eeacc0cc055568c6363a11db71a2dcb41c6
Glitch (aws-game-v2) <none>
2021-05-06 11:01:24 +0000

    🍁🎗 Updated with Glitch

--- views/header.js
+++ views/header.js
@@ -2,16 +2,16 @@
 * header
 */
 document.write("<div class=\"header\">");
-document.write("  <h1>WARZOMB<\/h1>");
-document.write("  <p>AWS project game<\/p>");
+document.write("  <h1>  <\/h1>");
+document.write("  <p>  <\/p>");
 document.write("<\/div>");
 document.write("");
 document.write("<div class=\"navbar\">");
-document.write("  <a href=\"index.html\">Accueil<\/a>");
-document.write("  <a href=\"profile.html\"  id=\"profile\">Profil<\/a>");
-document.write("  <a href=\"historique.html\" id=\"resume\">Historique<\/a>");
-document.write("  <a href=\"#\">Shop<\/a>");
-document.write("  <a href=\"game.html\">Play<\/a>");
-document.write("  <a href=\"\/login.html\" class=\"right_co\" id=\"right_co\">Inscription<\/a>");
-document.write("  <a href=\"\/index.html\" class=\"right_deco\" id=\"right_deco\"  onclick=\"mainApp.logOut()\" >Déconnexion<\/a>");
+document.write("  <a href=\"index.html\"  id=\"index\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/Accueil.png\"><\/a>");
+document.write("  <a href=\"profile.html\"  id=\"profile\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/Profil.png\"><\/a>");
+document.write("  <a href=\"historique.html\" id=\"resume\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/Hist.png\"><\/a>");
+document.write("  <a href=\"#\" id=\"shop\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/SHOP.png\"<\/a>");
+document.write("  <a href=\"game.html\" id=\"game\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/Play.png\"><\/a>");
+document.write("  <a href=\"\/login.html\" class=\"right_co\" id=\"right_co\"><img height=\"30px\" width=\"30px\" src=\"../tileset/images/login.png\"><\/a>");
+document.write("  <a href=\"\/index.html\" class=\"right_deco\" id=\"right_deco\"  onclick=\"mainApp.logOut()\" ><img height=\"30px\" width=\"30px\" src=\"../tileset/images/desc.png\"><\/a>");
 document.write("<\/div>");
